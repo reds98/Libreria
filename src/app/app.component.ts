212fd620a6633428a8e4112c393f3a47f0299e4f
Sahid Rojas Chacon <sahid.rojas64@gmail.com>
2024-10-19 11:17:22 -0600

    Se creo el componente tarjeta

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-
+import { TarjetaComponent } from './tarjeta/tarjeta.component'; 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,TarjetaComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
